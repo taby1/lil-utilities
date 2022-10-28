af35ae31e065bf039f7b604863ba85359c5f4cd2
Elaine <pyoon99@gmail.com>
2022-10-28 12:24:35 -0700

    works yay

--- dictionary-file-rename.py
+++ dictionary-file-rename.py
@@ -1,6 +1,6 @@
 import os, sys
 
-dd = {"Pilot":"S1E1 - Pilot", "Naming Rights":"S1E2 - Naming Rights", "YumTime":"S1E3 - YumTime", "Short Squeeze":"S1E4 - Short Squeeze", "The Good Life":"S1E5 - The Good Life", "The Deal":"S1E6 - The Deal", "The Punch":"S1E7 - The Punch", "Boasts and Rails":"S1E8 - Boasts and Rails", "Where the F*** is Donnie?":"S1E9 - Where the F*** is Donnie?", "Quality of Life":"S1E10 - Quality of Life", "Magical Thinking":"S1E11 - Magical Thinking", "The Conversation":"S1E12 - The Conversation", "Risk Management":"S2E1 - Risk Management", "Dead Cat Bounce":"S2E2 - Dead Cat Bounce", "Optimal Play":"S2E3 - Optimal Play", "The Oath":"S2E4 - The Oath", "Currency":"S2E5 - Currency", "Indian Four":"S2E6 - Indian Four", "Victory Lap":"S2E7 - Victory Lap", "The Kingmaker":"S2E8 - The Kingmaker", "Sic Transit Imperium":"S2E9 - Sic Transit Imperium", "With or Without You":"S2E10 - With or Without You", "Golden Frog Time":"S2E11 - Golden Frog Time", "Ball in Hand":"S2E12 - Ball in Hand", "Tie Goes to the Runner":"S3E1 - Tie Goes to the Runner", "The Wrong Maria Gonzalez":"S3E2 - The Wrong Maria Gonzalez", "A Generation Too Late":"S3E3 - A Generation Too Late", "Hell of a Ride":"S3E4 - Hell of a Ride", "Flaw in the Death Star":"S3E5 - Flaw in the Death Star", "The Third Ortolan":"S3E6 - The Third Ortolan", "Not You, Mr. Dake":"S3E7 - Not You, Mr. Dake", "All the Wilburys":"S3E8 - All the Wilburys", "Icebreaker":"S3E9 - Icebreaker", "Redemption":"S3E10 - Redemption", "Kompenso":"S3E11 - Kompenso", "Elmsley Count":"S3E12 - Elmsley Count", "Chucky Rhoades's Greatest Game":"S4E1 - Chucky Rhoades's Greatest Game", "Arousal Template":"S4E2 - Arousal Template", "Chickentown":"S4E3 - Chickentown", "Overton Window":"S4E4 - Overton Window", "A Proper Sendoff":"S4E5 - A Proper Sendoff", "Maximum Recreational Depth":"S4E6 - Maximum Recreational Depth", "Infinite Game":"S4E7 - Infinite Game", "Fight Night":"S4E8 - Fight Night", "American Champion":"S4E9 - American Champion", "New Year's Day":"S4E10 - New Year's Day", "Lamster":"S4E11 - Lamster", "Extreme Sandbox":"S4E12 - Extreme Sandbox", "The New Decas":"S4E1 - The New Decas", "The Chris Rock Test":"S5E2 - The Chris Rock Test", "Beg, Bribe, Bully":"S5E3 - Beg, Bribe, Bully", "Opportunity Zone":"S5E4 - Opportunity Zone", "Contract":"S5E5 - Contract", "The Nordic Model":"S5E6 - The Nordic Model", "The Limitless Shit":"S5E7 - The Limitless Shit", "Copenhagen":"S5E8 - Copenhagen", "Implosion":"S5E9 - Implosion", "Liberty":"S5E10 - Liberty", "Victory Smoke":"S5E11 - Victory Smoke", "No Direction Home":"S5E12 - No Direction Home", "Cannonade":"S6E1 - Cannonade", "Lyin' Eyes":"S6E2 - Lyin' Eyes", "STD":"S6E3 - STD", "Burn Rate":"S6E4 - Burn Rate", "Rock of Eye":"S6E5 - Rock of Eye", "Hostis Humani Generis":"S6E6 - Hostis Humani Generis", "Napoleon's Hat":"S6E7 - Napoleon's Hat", "The Big Ugly":"S6E8 - The Big Ugly", "Hindenburg":"S6E9 - Hindenburg", "Johnny Favorite":"S6E10 - Johnny Favorite", "Succession":"S6E11 - Succession", "Cold Storage":"S6E12 - Cold Storage"}
+dd = {"Grounded":"S3E1 - Grounded", "The Least Dangerous Game":"S3E2 - The Least Dangerous Game", "Mining the Mind's Mines":"S3E3 - Mining the Mind's Mines", "Room for Growth":"S3E4 - Room for Growth", "Reflections":"S3E5 - Reflections", "Hear All, Trust Nothing":"S3E6 - Hear All, Trust Nothing", "A Mathematically Perfect Redemption":"S3E7 - A Mathematically Perfect Redemption", "Crisis Point 2: Paradoxus":"S3E8 - Crisis Point 2: Paradoxus", "Trusted Sources":"S3E9 - Trusted Sources", "The Stars at Night":"S3E10 - The Stars at Night", "Asylum":"S3E11 - Asylum"}
 
 # get all files from directory
 
@@ -12,19 +12,25 @@ def get_files(directory):
 # main function
 if __name__ == "__main__":
     # get first commandline argument
-    directory = sys.argv[1]
+    if len(sys.argv) > 1:
+        directory = sys.argv[1]
+    else:
+        directory = r"Z:\Media\TV Shows\Star Trek Lower Decks"
     print(directory)
     # get all files from directory
     files = get_files(directory)
-    print(files)
+    # print(files)
     keys = dd.keys()
-    print(keys)
+    # print(keys)
     for file in files:
+        # print(any(key in file for key in keys))
         for key in keys:
-            if key in files:
-                print("Here")
+            if key in file:
                 # replace key substring with a dict value
                 print(file.replace(key, dd[key]))
+                new_path = os.path.join(directory, file.replace(key, dd[key]))
+                old_path = os.path.join(directory, file)
+                os.rename(old_path, new_path)
     #     # check if key from dict is in file name
     #     # get filename
 
